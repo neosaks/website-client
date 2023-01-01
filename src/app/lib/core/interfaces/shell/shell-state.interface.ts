25bf2e3a0bc00cc05251fdaebf8a9360cbd873fa
Maxim Chichkanov <neosaks@mail.ru>
2023-01-02 02:20:04 +0300

    Удаление устаревшего интерфейса

--- src/app/lib/core/interfaces/shell/shell-state.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IShellState {
-  /**
-   * The name of the application is displayed on the toolbar.
-   * The space character is the default value. This is necessary for
-   * the state mechanism to work properly using the state property.
-   */
-  appName: string;
-}
